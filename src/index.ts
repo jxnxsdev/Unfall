aa5a196184b62dbef8413233b316114e98806ca3
hdgamer1404Jonas <hdgamer1404Jonas@gmail.com>
2023-09-21 15:49:54 +0200

    api endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,110 @@
-import Koa from 'koa';
+import Fastify from 'fastify'
 import path from 'path';
-import serve from 'koa-static';
+import fs from 'fs';
+import puller from './puller';
+import { PrismaClient} from '@prisma/client';
+import { response } from 'express';
 
-const app = new Koa();
-const port = 3000; // Wählen Sie den gewünschten Port aus
+let prisma: PrismaClient;
 
-// Statische Dateien im "public" Verzeichnis bereitstellen (zum Beispiel CSS, Bilder usw.)
-app.use(serve(path.join(__dirname, '..', 'html')));
+const fastify = Fastify({
+  logger: true
+})
 
-// Pfad "/" behandeln und die HTML-Datei senden
-app.use(async (ctx) => {
-  ctx.type = 'html';
-  ctx.body = await (require('fs')).createReadStream(path.join(__dirname, '..', 'html', 'index.html'));
+fastify.get('/', async (request, reply) => {
+  reply.type('html').code(200)
+  return fs.createReadStream(path.join(__dirname, '../html/index.html'))
+})
+
+fastify.get('/api/get', async (request, reply) => {
+    // get the crashes from the last 30 days
+    let prisma = await getPrisma();
+    let crashes = await prisma.crashReports.findMany({
+        where: {
+            report_date: {
+                gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000)
+            }
+        }
+    });
+
+    let crashData = [];
+
+    for (let crash of crashes) {
+        let crashDataEntry = {
+            username: crash.username,
+            id: crash.id,
+            report_date: crash.report_date,
+            gameversion: crash.gameversion
+        };
+
+        crashData.push(crashDataEntry);
+    }
+
+    reply.type('application/json').code(200);
+
+    return crashData;
+})
+
+fastify.get('/api/getusers', async (request, reply) => {
+  // get the amount of crashes from the top 10 users
+  let prisma = await getPrisma();
+  let users = await prisma.users.findMany({
+      orderBy: {
+          crash_count: 'desc'
+      },
+      take: 10
+    });
+
+  let userData = [];
+
+  for (let user of users) {
+      let userDataEntry = {
+          username: user.username,
+          crash_count: user.crash_count
+      };
+
+      userData.push(userDataEntry);
+  }
+
+  reply.type('application/json').code(200);
+
+  return userData;
+
 });
 
-app.listen(port, () => {
-  console.log(`Server läuft auf http://localhost:${port}`);
+fastify.get('/api/getallusers', async (request, reply) => {
+  // get the users crash counts ordered by crash count
+  let prisma = await getPrisma();
+  let users = await prisma.users.findMany({
+      orderBy: {
+          crash_count: 'desc'
+      }
+    });
+
+  let userData = [];
+
+  for (let user of users) {
+      let userDataEntry = {
+          username: user.username,
+          crash_count: user.crash_count
+      };
+
+      userData.push(userDataEntry);
+  }
+
+  reply.type('application/json').code(200);
+
+  return userData;
 });
+
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) throw err
+  fastify.log.info(`server listening on ${address}`)
+  
+  prisma = new PrismaClient();
+  puller();
+})
+
+export default async function getPrisma() {
+    return prisma;
+}
