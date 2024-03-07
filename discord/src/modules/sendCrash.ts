76687dc06816e5ed322af89cca1e424c26d6fa24
hdgamer1404Jonas <hdgamer1404Jonas@gmail.com>
2024-03-07 18:18:06 +0100

    Code improvements

--- discord/src/modules/sendCrash.ts
+++ discord/src/modules/sendCrash.ts
@@ -3,76 +3,58 @@ import { client } from '..';
 import * as types from '../types';
 
 export async function sendCrash(crash: types.processed_crash) {
-    let data: types.data = JSON.parse(crash.data);
+    const data: types.data = JSON.parse(crash.data);
+    // @ts-ignore
+    const modString = data.mods.map(mod => `${mod.name}:${mod.version}`).join(', ');
 
-    let modString = ``;
+    const isLiveness = data.causes.includes("liveness");
+    const isHook = data.causes.includes("hook");
+    const isCrashMod = data.causes.includes("libcrashmod.so");
 
-    for (let i = 0; i < data.mods.length; i++) {
-        // @ts-ignore
-        modString += `${data.mods[i].name}:${data.mods[i].version}, `;
-    }
+    const time = new Date(crash.crash_time);
+    const unix = Math.floor(time.getTime() / 1000);
 
-    modString = modString.slice(0, -2);
+    let description = `
+        **Player ID:** ${crash.player_id}
+        **Crash Time:** <t:${unix}:R>
+        **Version:** ${crash.version}
 
-    let embed = new discord.EmbedBuilder()
-    .setTitle("New Crash: " + crash.crash_id);
+        **Mods:**
+        ${modString}
+    `;
 
-    if (data.causes.includes("liveness")) {
-        embed.setDescription(`
-            **LIVENESS CRASH**
-
-            **Player ID:** ${crash.player_id}
-            **Crash Time:** ${crash.crash_time}
-            **Version:** ${crash.version}
-
-            **Mods:**
-            ${modString}
-        `)
-    }else if (data.causes.includes("hook")) {
-        embed.setDescription(`
-            **HOOK CRASH**
-
-            **Player ID:** ${crash.player_id}
-            **Crash Time:** ${crash.crash_time}
-            **Version:** ${crash.version}
-
-            **Mods:**
-            ${modString}
-        `)
-    } if (data.causes.includes("libCrashMod.so")) {
-        embed.setDescription(`
-            **Looks like ${crash.player_id} was screwed over by crashmod**
-        `);
-        embed.setImage("https://media1.tenor.com/m/9ccpdUqcvXIAAAAC/iu.gif")
+    if (isLiveness) {
+        description = "**LIVENESS CRASH**\n\n" + description;
+    } else if (isHook) {
+        description = "**HOOK CRASH**\n\n" + description;
+    } else if (isCrashMod) {
+        description = `**Looks like ${crash.player_id} was screwed over by crashmod**`;
     } else {
-        embed.setDescription(`
-            **Player ID:** ${crash.player_id}
-            **Crash Time:** ${crash.crash_time}
-            **Version:** ${crash.version}
-
-            **Mods:**
-            ${modString}
-
-            **Coulprits:**
-            ${data.causes.join('\n')}
-        `)
+        description += `\n\n**Coulprits:**\n${data.causes.join('\n')}`;
     }
 
-    embed.setColor("#FF0000");
-    embed.setURL(`https://analyzer.questmodding.com/crashes/${crash.crash_id}`);
-    
-    const channels = process.env.CRASH_CHANNELS.split(',');
+    const embed = new discord.EmbedBuilder()
+        .setTitle("New Crash: " + crash.crash_id)
+        .setDescription(description)
+        .setColor("#FF0000")
+        .setURL(`https://analyzer.questmodding.com/crashes/${crash.crash_id}`);
 
-    sendEmbed(embed).catch(console.error);
+    if (isCrashMod) {
+        embed.setImage("https://media1.tenor.com/m/9ccpdUqcvXIAAAAC/iu.gif");
+    }
+
+    await sendEmbed(embed);
 }
 
 async function sendEmbed(embed: discord.EmbedBuilder) {
     const channels = process.env.CRASH_CHANNELS.split(',');
 
     for (const channel of channels) {
-        let c = await client.channels.fetch(channel) as discord.TextChannel;
-        await c.send({
-            embeds: [embed]
-        }).catch(console.error);
+        try {
+            const c = await client.channels.fetch(channel) as discord.TextChannel;
+            await c.send({ embeds: [embed] });
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
